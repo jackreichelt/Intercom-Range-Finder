e0c2b975ead89d9269ba1db3a8dfb5fef65fcf13
jackreichelt <jackreichelt@gmail.com>
2018-03-06 22:07:19 +1100

    Worked out what was going wrong. Was converting to radians at the wrong point.

--- distanceCalculatorTest.py
+++ distanceCalculatorTest.py
@@ -11,21 +11,21 @@ class TestDistanceMethods(unittest.TestCase):
         self.assertEqual(greatCircleDistance(OFFICE), 0) #shouldn't pass if the previous failed
         
     def test_nearbyPosition(self):
-        NEARBY = {'name': 'St Stevens Green', #The dead centre of St Stevens Green. approx 200m
-                  'latitude': 53.338158,
-                  'longitude': -6.259138} 
+        NEARBY = {'name': 'St Stevens Green', #The centre of St Stevens Green. approx 200m
+                  'latitude': '53.338158',
+                  'longitude': '-6.259138'}
         self.assertLess(greatCircleDistance(NEARBY), .250) #shouldn't pass if the previous failed
     
     def test_distantPosition(self):
         DISTANT = {'name': 'My apartment', #My apartment in Sydney, approx 17,215 km
-                  'latitude': -33.883449,
-                  'longitude': 151.181398} 
+                  'latitude': '-33.883449',
+                  'longitude': '151.181398'}
         self.assertGreater(greatCircleDistance(DISTANT), 17215)
         self.assertLess(greatCircleDistance(DISTANT), 17300)
         
     def test_zeroPosition(self):
-        ZERO = {'latitude': 0, #0 lat and long, approx 5,959 km
-                'longitude': 0}
+        ZERO = {'latitude': '0', #0 lat and long, approx 5,959 km
+                'longitude': '0'}
         self.assertGreater(greatCircleDistance(ZERO), 5900)
         self.assertLess(greatCircleDistance(ZERO), 6000)
     

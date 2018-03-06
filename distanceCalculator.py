1ea6cccc42d37555af8a30b080799351fded8cbb
jackreichelt <jackreichelt@gmail.com>
2018-03-06 21:19:19 +1100

    Wrote distanceCalculator functions. Noting a weird bug, for distant points. Close ones appear to be working fine. More testing required.

--- distanceCalculator.py
+++ distanceCalculator.py
@@ -1,11 +1,18 @@
 from math import acos, cos, sin, radians
 
 RADIUS = 6371 #assuming a spherical earth
-OFFICE = {'latitude': 53.339428, 'longitude': -6.257664}
+OFFICE = {'latitude': 53.339428, 'longitude': -6.257664} #Intercom location
 
 # Returns the angle between two points on the earth
 def angleBetweenPoints(pointA, pointB):
-    pass
+    phi1 = pointA['latitude']
+    phi2 = float(pointB['latitude'])
+    lam1 = pointA['longitude']
+    lam2 = float(pointB['longitude'])
     
+    longitudeDifference = abs(lam1 - lam2)
+    
+    return acos(sin(phi1) * sin(phi2) + cos(phi1) * cos(phi2) * cos(longitudeDifference))
+
 def greatCircleDistance(targetPoint):
-    pass
+    return RADIUS * radians(angleBetweenPoints(OFFICE, targetPoint))
--- distanceCalculatorTest.py
+++ distanceCalculatorTest.py
@@ -7,21 +7,27 @@ OFFICE = {'latitude': 53.339428, 'longitude': -6.257664} #Intercom location
 class TestDistanceMethods(unittest.TestCase):
     
     def test_samePosition(self):
-        self.assertEqual(greatCircleDistance(OFFICE), 0)
-        self.assertEqual(angleBetweenPoints(OFFICE, OFFICE), 0) #shouldn't pass if the previous failed
+        self.assertEqual(angleBetweenPoints(OFFICE, OFFICE), 0)
+        self.assertEqual(greatCircleDistance(OFFICE), 0) #shouldn't pass if the previous failed
         
     def test_nearbyPosition(self):
         NEARBY = {'name': 'St Stevens Green', #The dead centre of St Stevens Green. approx 200m
                   'latitude': 53.338158,
                   'longitude': -6.259138} 
-        self.assertLess(greatCircleDistance(NEARBY), .250)
-        self.assertLess(angleBetweenPoints(OFFICE, NEARBY), .250) #shouldn't pass if the previous failed
+        self.assertLess(greatCircleDistance(NEARBY), .250) #shouldn't pass if the previous failed
     
-    def test_distancePosition(self):
-        NEARBY = {'name': 'My apartment', #My apartment in Sydney, approx 17,215 km
+    def test_distantPosition(self):
+        DISTANT = {'name': 'My apartment', #My apartment in Sydney, approx 17,215 km
                   'latitude': -33.883449,
                   'longitude': 151.181398} 
-        self.assertGreater(greatCircleDistance(NEARBY), 17200)
+        self.assertGreater(greatCircleDistance(DISTANT), 17215)
+        self.assertLess(greatCircleDistance(DISTANT), 17300)
+        
+    def test_zeroPosition(self):
+        ZERO = {'latitude': 0, #0 lat and long, approx 5,959 km
+                'longitude': 0}
+        self.assertGreater(greatCircleDistance(ZERO), 5900)
+        self.assertLess(greatCircleDistance(ZERO), 6000)
     
 if __name__ == '__main__':
     unittest.main()

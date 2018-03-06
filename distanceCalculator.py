60097a55c14f803f34c1a91b67d354116f755b28
jackreichelt <jackreichelt@gmail.com>
2018-03-06 18:48:43 +1100

    Added in the data file, and the skeleton of the distance calculator

---
+++ distanceCalculator.py
@@ -0,0 +1,11 @@
+from math import acos, cos, sin, radians
+
+RADIUS = 6371 #assuming a spherical earth
+OFFICE = {'latitude': 53.339428, 'longitude': -6.257664}
+
+# Returns the angle between two points on the earth
+def angleBetweenPoints(pointA, pointB):
+    pass
+    
+def greatCircleDistance(targetPoint):
+    pass
